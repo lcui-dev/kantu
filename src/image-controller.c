1facbb8263640cc86811c0fdad15425090d5e37b
Liu <lc-soft@live.cn>
2024-07-21 16:28:35 +0800

    添加控制器

---
+++ src/image-controller.c
@@ -0,0 +1,126 @@
+#include "image-controller.h"
+
+#define SCALE_MAX 10.f
+#define SCALE_STEP 0.2f
+
+void image_controller_init(image_controller_t *c)
+{
+        c->scale = 1.f;
+        c->min_scale = 1.f;
+        c->image = NULL;
+        c->image_offset_x = 0;
+        c->image_offset_y = 0;
+        c->viewport_center_x = 0;
+        c->viewport_center_y = 0;
+        c->viewport_width = 0;
+        c->viewport_height = 0;
+}
+
+void image_controller_destroy(image_controller_t *c)
+{
+        if (c->image) {
+                ui_image_destroy(c->image);
+        }
+        c->image = NULL;
+}
+
+void image_controller_set_position(image_controller_t *c, float image_offset_x,
+                                   float image_offset_y)
+{
+        pd_canvas_t *data = &c->image->data;
+        float image_width = c->scale * data->width;
+        float image_height = c->scale * data->height;
+
+        if (image_width <= c->viewport_width) {
+                image_offset_x = (c->viewport_width - image_width) / 2.f;
+        } else if (image_offset_x > 0) {
+                image_offset_x = 0;
+        } else if (c->viewport_width - image_offset_x > image_width) {
+                image_offset_x = c->viewport_width - image_width;
+        }
+        if (image_height <= c->viewport_height) {
+                image_offset_y = (c->viewport_height - image_height) / 2.f;
+        } else if (image_offset_y > 0) {
+                image_offset_y = 0;
+        } else if (c->viewport_height - image_offset_y > image_height) {
+                image_offset_y = c->viewport_height - image_height;
+        }
+        c->image_offset_x = image_offset_x;
+        c->image_offset_y = image_offset_y;
+        c->viewport_center_x =
+            (-c->image_offset_x + c->viewport_width / 2.f) / c->scale;
+        c->viewport_center_y =
+            (-c->image_offset_y + c->viewport_height / 2.f) / c->scale;
+}
+
+float image_controller_compute_zoom_to_fill(image_controller_t *c)
+{
+        pd_canvas_t *data = &c->image->data;
+        float width_scale = data->width > c->viewport_width
+                                ? c->viewport_width / data->width
+                                : 1.f;
+        float height_scale = data->height > c->viewport_height
+                                 ? c->viewport_height / data->height
+                                 : 1.f;
+
+        return width_scale < height_scale ? width_scale : height_scale;
+}
+
+void image_controller_set_scale(image_controller_t *c, float scale)
+{
+        if (scale < c->min_scale) {
+                scale = c->min_scale;
+        }
+        if (scale > SCALE_MAX) {
+                scale = SCALE_MAX;
+        }
+        c->scale = scale;
+        image_controller_set_position(
+            c, -(c->viewport_center_x * c->scale - c->viewport_width / 2.f),
+            -(c->viewport_center_y * c->scale - c->viewport_height / 2.f));
+}
+
+void image_controller_reset_scale(image_controller_t *c)
+{
+        c->min_scale = image_controller_compute_zoom_to_fill(c);
+        image_controller_set_scale(c, c->min_scale);
+}
+
+void image_controller_zoom_in(image_controller_t *c)
+{
+        image_controller_set_scale(c, c->scale + SCALE_STEP);
+}
+
+void image_controller_zoom_out(image_controller_t *c)
+{
+        image_controller_set_scale(c, c->scale - SCALE_STEP);
+}
+
+void image_controller_zoom_to_fill(image_controller_t *c)
+{
+        image_controller_set_scale(c, image_controller_compute_zoom_to_fill(c));
+}
+
+bool image_controller_can_zoom_in(image_controller_t *c)
+{
+        return ui_image_valid(c->image) && c->scale < SCALE_MAX;
+}
+
+bool image_controller_can_zoom_out(image_controller_t *c)
+{
+        return ui_image_valid(c->image) && c->scale > c->min_scale;
+}
+
+bool image_controller_can_zoom_to_fill(image_controller_t *c)
+{
+        return ui_image_valid(c->image) &&
+               image_controller_compute_zoom_to_fill(c) != c->scale;
+}
+
+void image_controller_load_file(image_controller_t *c, const char *file)
+{
+        if (c->image) {
+                ui_image_destroy(c->image);
+        }
+        c->image = ui_image_create(file);
+}
---
+++ src/image-controller.h
@@ -0,0 +1,37 @@
+#include <ui.h>
+
+typedef struct image_controller {
+        float scale;
+        float min_scale;
+        float viewport_width, viewport_height;
+        float viewport_center_x, viewport_center_y;
+        float image_offset_x, image_offset_y;
+        ui_image_t *image;
+} image_controller_t;
+
+void image_controller_init(image_controller_t *c);
+
+void image_controller_destroy(image_controller_t *c);
+
+void image_controller_set_position(image_controller_t *c, float image_offset_x,
+                                   float image_offset_y);
+
+float image_controller_compute_zoom_to_fill(image_controller_t *c);
+
+void image_controller_set_scale(image_controller_t *c, float scale);
+
+void image_controller_reset_scale(image_controller_t *c);
+
+void image_controller_zoom_in(image_controller_t *c);
+
+void image_controller_zoom_out(image_controller_t *c);
+
+void image_controller_zoom_to_fill(image_controller_t *c);
+
+bool image_controller_can_zoom_in(image_controller_t *c);
+
+bool image_controller_can_zoom_out(image_controller_t *c);
+
+bool image_controller_can_zoom_to_fill(image_controller_t *c);
+
+void image_controller_load_file(image_controller_t *c, const char *file);
